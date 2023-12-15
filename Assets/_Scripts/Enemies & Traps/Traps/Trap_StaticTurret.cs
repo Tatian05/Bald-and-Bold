73bf046e68d1b5f1551d7f89e1fe3a47fb655a42
Tatian05 <isp2014asje@gmail.com>
2023-12-15 12:10:39 -0300

    Torreta

--- Assets/_Scripts/Enemies & Traps/Traps/Trap_StaticTurret.cs
+++ Assets/_Scripts/Enemies & Traps/Traps/Trap_StaticTurret.cs
@@ -1,10 +1,9 @@
-using System.Collections;
 using UnityEngine;
 using System;
-
+using DG.Tweening;
 public class Trap_StaticTurret : MonoBehaviour
 {
-    [SerializeField] Transform _shootingPoint;
+    [SerializeField] Transform _shootingPoint, _cannon;
     [SerializeField] ParticleSystem _sparksShootPT;
     [SerializeField] float _damage = 1;
     [SerializeField] float _bulletSpeed = 5;
@@ -36,6 +35,7 @@ public class Trap_StaticTurret : MonoBehaviour
                                                 .SetDirection(_shootingPoint.right)
                                                 .SetDmg(_damage)
                                                 .SetSpeed(_bulletSpeed);
+            _cannon.DOLocalMoveX(-.25f, .1f).SetEase(Ease.OutSine).SetLoops(2, LoopType.Yoyo);
             _currentAttackSpeed = 0;
         }
     }
