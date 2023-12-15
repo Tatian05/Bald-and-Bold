11c16641e8a20547a2ab2411bcd3ebb6f3a2e6db
Tatian05 <isp2014asje@gmail.com>
2023-12-15 18:59:04 -0300

    Rework plataforma

--- Assets/_Scripts/Platforms/Platform_Disappear.cs
+++ Assets/_Scripts/Platforms/Platform_Disappear.cs
@@ -1,43 +1,51 @@
-using System.Collections;
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class Platform_Disappear : MonoBehaviour
 {
     public Action OnUpdate;
 
-    [SerializeField] float _timeToDisappear;
     [SerializeField] float _timeToAppear;
-
-    float _currentTimeToAppear;
-    float _currentTimeToDisappear;
-
+    [SerializeField] AnimationClip _defaultAnimationClip;
     [SerializeField] GameObject _sprite;
     [SerializeField] ParticleSystem _dustPS;
 
+    float _disappeatTimer, _startAppearTimer;
+    float _disappearAnimDuration;
     Collider2D[] _colliders;
     Animator _anim;
-
-    bool _isDisappearing = false;
-
+    bool _shaking;
+    ParticleSystem.MainModule _psModule;
     private void Start()
     {
         _anim = GetComponent<Animator>();
         _colliders = GetComponents<BoxCollider2D>();
+        _disappearAnimDuration = _defaultAnimationClip.length;
+        _psModule = _dustPS.main;
 
         EventManager.SubscribeToEvent(Contains.PLAYER_DEAD, AppearPlatform);
     }
-    private void OnDisable()
+    private void OnDestroy()
     {
-        EventManager.UnSubscribeToEvent(Contains.PLAYER_DEAD, AppearPlatform);       
+        EventManager.UnSubscribeToEvent(Contains.PLAYER_DEAD, AppearPlatform);
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player") && !_isDisappearing)
+        if (collision.CompareTag("Player"))
         {
-            OnUpdate += CheckDisappear;
-            _isDisappearing = true;
-            _anim.Play("Breaking");
+            OnUpdate = AddCracks;
+            if (!_shaking) StartCoroutine(Shake());
+            _anim.SetFloat("Disappear", 1);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (_disappeatTimer < _disappearAnimDuration) OnUpdate = LessCracks;
+            _anim.SetFloat("Disappear", -1);
         }
     }
 
@@ -46,53 +54,75 @@ public class Platform_Disappear : MonoBehaviour
         OnUpdate?.Invoke();
     }
 
-    void CheckAppear()
+    void LessCracks()
     {
-        _currentTimeToAppear += Time.deltaTime;
+        if (_disappeatTimer <= 0)
+            return;
 
-        if (_currentTimeToAppear < _timeToAppear) return;
+        _anim.SetBool("Idle", _disappeatTimer <= 0);
+        _psModule.maxParticles = (int)Mathf.Lerp(0, 50, _disappeatTimer / _disappearAnimDuration);
+        _disappeatTimer -= Time.deltaTime;
+    }
+    void AddCracks()
+    {
+        if (_disappeatTimer > _disappearAnimDuration)
+        {
+            DisappearPlatform();
+            return;
+        }
+        _anim.SetBool("Idle", _disappeatTimer <= 0);
+        _psModule.maxParticles = (int)Mathf.Lerp(0, 50, _disappeatTimer / _disappearAnimDuration);
+        _disappeatTimer += Time.deltaTime;
+    }
 
-        AppearPlatform();
+    void AppearPlatform(params object[] param)
+    {
+        _sprite.SetActive(true);
+        _psModule.maxParticles = 0;
+        _disappeatTimer = 0;
+        _startAppearTimer = 0;
+        OnUpdate = null;
+
+        foreach (var item in _colliders)
+            item.enabled = true;
+    }
+    void StartAppearPlatform()
+    {
+        _startAppearTimer += Time.deltaTime;
+        if (_startAppearTimer >= _timeToAppear) AppearPlatform();
+    }
+    void DisappearPlatform()
+    {
+        _anim.SetBool("Idle", true);
+        _psModule.maxParticles = 0;
+        OnUpdate = StartAppearPlatform;
+        _sprite.SetActive(false);
+
+        foreach (var item in _colliders)
+            item.enabled = false;
     }
     public void PlayDust()
     {
         _dustPS.Play();
     }
-    void CheckDisappear()
+    IEnumerator Shake()
     {
-        _currentTimeToDisappear += Time.deltaTime;
+        yield return null;
+        Vector3 startPosition = transform.position;
+        _shaking = true;
 
-        if (_currentTimeToDisappear < _timeToDisappear) return;
-
-        DisappearPlatform();
-        _currentTimeToDisappear = 0;
-        OnUpdate += CheckAppear;
-        OnUpdate -= CheckDisappear;
-    }
-
-    void AppearPlatform(params object[] param)
-    {
-        OnUpdate = null;
-        _currentTimeToAppear = 0;
-        _currentTimeToDisappear = 0;
-
-        _sprite.SetActive(true);
-
-        foreach (var item in _colliders)
+        while (_disappeatTimer < _disappearAnimDuration)
         {
-            item.enabled = true;
-        }
-        _anim.Play("Idle");
-        _isDisappearing = false;
-    }
+            float x = UnityEngine.Random.Range(-1f, 1f) * _disappeatTimer * .05f;
+            float y = UnityEngine.Random.Range(-1f, 1f) * _disappeatTimer * .05f;
 
-    void DisappearPlatform()
-    {
-        _sprite.SetActive(false);
+            transform.position = new Vector3(startPosition.x + x, startPosition.y + y, startPosition.z);
 
-        foreach (var item in _colliders)
-        {
-            item.enabled = false;
+            if (_disappeatTimer <= 0f) break;
+            yield return null;
         }
+
+        transform.position = startPosition;
+        _shaking = false;
     }
 }
