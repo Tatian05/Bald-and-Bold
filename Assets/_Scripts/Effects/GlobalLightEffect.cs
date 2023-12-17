bc5bf60f3e08fb9308356b603ad0f8af52c93d51
Tatian05 <isp2014asje@gmail.com>
2023-12-16 23:14:44 -0300

    asd

--- Assets/_Scripts/Effects/GlobalLightEffect.cs
+++ Assets/_Scripts/Effects/GlobalLightEffect.cs
@@ -7,11 +7,9 @@ public class GlobalLightEffect : MonoBehaviour
     Volume _volume;
     ColorAdjustments _colorAdjustments;
     LevelTimerManager _levelTimerManager;
-    int _index;
     EventFSM<ColorPhases> _myFsm;
 
     [SerializeField, ColorUsage(true, true)] Color _signalColor;
-    float[] _frecuenciaParpadeo = new float[7];
     void Start()
     {
         _levelTimerManager = Helpers.LevelTimerManager;
@@ -20,17 +18,13 @@ public class GlobalLightEffect : MonoBehaviour
         _levelTimerManager.RedButton += OnLevelFinish;
         _levelTimerManager.OnLevelDefeat += OnLevelFinish;
 
-        _frecuenciaParpadeo = new float[10] { _levelTimerManager.LevelMaxTime - 10,  _levelTimerManager.LevelMaxTime - 9, _levelTimerManager.LevelMaxTime - 8,
-        _levelTimerManager.LevelMaxTime - 7 ,_levelTimerManager.LevelMaxTime - 6, _levelTimerManager.LevelMaxTime - 5, _levelTimerManager.LevelMaxTime - 4,
-        _levelTimerManager.LevelMaxTime - 3, _levelTimerManager.LevelMaxTime - 2, _levelTimerManager.LevelMaxTime - 1 };
-
         var goingRed = new State<ColorPhases>("GoingRed");
         var goingWhite = new State<ColorPhases>("GoingWhite");
         var empty = new State<ColorPhases>("Empty");
 
         StateConfigurer.Create(empty).SetTransition(ColorPhases.GoingRed, goingRed).Done();
         StateConfigurer.Create(goingRed).SetTransition(ColorPhases.GoingWhite, goingWhite).Done();
-        StateConfigurer.Create(goingWhite).SetTransition(ColorPhases.Empty, empty).Done();
+        StateConfigurer.Create(goingWhite).SetTransition(ColorPhases.GoingRed, goingRed).Done();
 
         float timer = 0;
 
@@ -48,17 +42,14 @@ public class GlobalLightEffect : MonoBehaviour
             timer += CustomTime.DeltaTime;
             _colorAdjustments.colorFilter.value = Color.Lerp(_signalColor, Color.white, timer / .5f);
 
-            if (timer / 0.5f >= 1) _myFsm.SendInput(ColorPhases.Empty);
+            if (timer / 0.5f >= 1) _myFsm.SendInput(ColorPhases.GoingRed);
         };
         goingWhite.OnExit += delegate { timer = 0; };
 
         empty.OnUpdate += delegate
         {
-            if (_levelTimerManager.Timer >= _frecuenciaParpadeo[_index])
-            {
+            if (_levelTimerManager.Timer >= _levelTimerManager.LevelMaxTime - 10)
                 _myFsm.SendInput(ColorPhases.GoingRed);
-                _index++;
-            }
         };
 
         _myFsm = new EventFSM<ColorPhases>(empty);
@@ -71,7 +62,6 @@ public class GlobalLightEffect : MonoBehaviour
     {
         _myFsm = null;
         _colorAdjustments.colorFilter.value = Color.white;
-        _index = 0;
     }
     private void OnDestroy()
     {
